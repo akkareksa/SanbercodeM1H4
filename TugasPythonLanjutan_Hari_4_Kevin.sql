0f6b5007068ae628eed3eb81bafb1d7436c57eab
Kevin <vckevin98@gmail.com>
2020-11-05 22:35:36 +0700

    tugas sql hari 4

---
+++ TugasPythonLanjutan_Hari_4_Kevin.sql
@@ -0,0 +1,13 @@
+SELECT a.Title, b.Name, t.Name
+FROM albums a
+JOIN artists b
+	ON a.ArtistId = b.ArtistId
+JOIN tracks t
+	ON t.AlbumId = a.AlbumId
+WHERE 
+	b.Name = "Aerosmith" OR 
+	(
+		b.Name = "AC/DC" AND
+		a.Title = "Let There Be Rock"
+	)
+	
